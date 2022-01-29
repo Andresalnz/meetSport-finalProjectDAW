542ef3f6f07b867bfa9ed3483a412dde0f343d5e
Andres Aleu <andresaleunz@gmail.com>
2022-01-29 21:26:59 +0100

    Add useEffect hook listPublication

--- front/src/hooks/useListRequest.js
+++ front/src/hooks/useListRequest.js
@@ -3,17 +3,13 @@ import {useState, useEffect} from 'react'
 export default function useListRequest() {
 
   const [listRequest, setlistRequest] = useState([])
-  const callback = () => {
+  useEffect(() => {
     fetch('http://localhost:3001/publications')
     .then(result => result.json()) 
     .then(result => {
       setlistRequest(result)
-      console.log(result)
     })
-    return () => {}//Se ejecuta cuando el componente se destruye
-  }
-
-  callback()
+  },[])
 
   return listRequest
 }
