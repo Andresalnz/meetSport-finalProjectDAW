6acacfa91a1beb29ae194f91cfd7fa9cd740661c
Andres Aleu <andresaleunz@gmail.com>
2022-02-24 22:58:34 +0100

    Change useAuth

--- front/src/hooks/useSigIn.js
+++ front/src/hooks/useSigIn.js
@@ -6,7 +6,7 @@ import { useAuth } from './useAuth';
 export default function useSignIn() {
   const navigate = useNavigate();
   const [signInErrorState, setSignInError] = useState({error:false, message:'OK'});
-  const {token, setToken} = useAuth()
+  const {setUser} = useAuth({})
   
 
   const url = 'http://localhost:3001/signin'
@@ -32,7 +32,8 @@ export default function useSignIn() {
       
     .then((result) =>{
       sessionStorage.setItem('token', result.token)
-      setToken(result.token)
+      sessionStorage.setItem('userId', result.id)
+      setUser({token: result.token, userId: result.id})
       setSignInError({error:false, message:'OK'})
       navigate('/')
     })
