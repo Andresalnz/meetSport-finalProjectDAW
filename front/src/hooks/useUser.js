7216c77e38ae0433febd935f51725521c19a3497
Andres Aleu <andresaleunz@gmail.com>
2022-02-27 03:33:37 +0100

    hook return user active

---
+++ front/src/hooks/useUser.js
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react'
+import { useAuth } from './useAuth';
+
+export default function useListRequest() {
+  const {user} = useAuth()
+
+  const [userObject, setUser] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:3001/user/${user.userId}`)
+    .then(result => result.json()) 
+    .then(result => {
+      console.log("que devuelves ", result)
+      setUser(result)
+    })
+  },[])
+
+  return userObject
+}
