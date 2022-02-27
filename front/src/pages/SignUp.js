8e8235e4b7a7c5e22dea2427959d7cc660fde005
Andres Aleu <andresaleunz@gmail.com>
2022-02-27 11:54:23 +0100

    Change names inputs and add hook

--- front/src/pages/SignUp.js
+++ front/src/pages/SignUp.js
@@ -18,8 +18,16 @@ import {
 } from 'grommet';
 import { deepMerge } from 'grommet/utils';
 import { grommet } from 'grommet/themes';
+import useListSports  from '../hooks/useListSports'
+import useCreateUser from '../hooks/useCreateUser';
+
 
 export default function SignUp(props) {
+    const listSports = useListSports()
+
+    const sports=listSports.map((i)=>{ return i.name  })
+
+    const sendRequest = useCreateUser();
 
     
     return (
@@ -29,18 +37,21 @@ export default function SignUp(props) {
                     SignUp
                 </Heading>
                 {/* user information*/}
-                    <Form>
-                        <FormField name='text' label='Username' >
-                            <TextInput id="username" placeholder='Username' required></TextInput>
+                    <Form
+                    onSubmit={({ value }) => sendRequest(value)}>
+  
+     
+                        <FormField name='username' label='Username' >
+                            <TextInput name='username' id="username" placeholder='Username' required></TextInput>
                         </FormField>
-                        <FormField name='text' label='Mail' margin={{top:"20px"}}>
-                            <TextInput id="username" placeholder='Mail' required></TextInput>
+                        <FormField name='mail' label='Mail' margin={{top:"20px"}}>
+                            <TextInput name='mail'id="username" placeholder='Mail' required></TextInput>
                         </FormField>
-                        <FormField name='text' label='Contraseña' margin={{top:"20px"}}>
-                            <TextInput id="username" placeholder='Contraseña' required></TextInput>
+                        <FormField name='password' label='Contraseña' margin={{top:"20px"}}>
+                            <TextInput name='password' id="password" placeholder='Contraseña' required></TextInput>
                         </FormField>
-                        <FormField name='text' label='Ciudad' margin={{top:"20px"}}>
-                            <TextInput id="username" placeholder='Ciudad' required></TextInput>
+                        <FormField name='location' label='Ciudad' margin={{top:"20px"}}>
+                            <TextInput name='location' id="username" placeholder='Ciudad' required></TextInput>
                         </FormField>
                 {/*Sports */}
                         <Box align='start'  margin={{top:'20px'}}>
@@ -48,9 +59,9 @@ export default function SignUp(props) {
                                 <FormField name="subscribe">
                                     <Text id="chooseSport">Elige deportes (máximo 3)</Text>
                                     <Box direction='row' margin={{vertical:"10px"}}>
-                                        <CheckBoxGroup name="sport" margin={{right:'10px'}}  options={["Baloncesto","Fútbol","Runner"]} direction='column'/> 
-                                        <CheckBoxGroup name="sport" margin={{right:'10px'}}  options={["Baloncesto","Fútbol","Runner"]} direction='column'/>
-                                        <CheckBoxGroup name="sport" margin={{right:'10px'}}  options={["Baloncesto","Fútbol","Runner"]} direction='column'/>
+                                        <CheckBoxGroup name="sportName" margin={{right:'10px'}}  options={sports} direction='column'/> 
+                                        {/* <CheckBoxGroup name="sport" margin={{right:'10px'}}  options={["Baloncesto","Fútbol","Runner"]} direction='column'/>
+                                        <CheckBoxGroup name="sport" margin={{right:'10px'}}  options={["Baloncesto","Fútbol","Runner"]} direction='column'/> */}
                                     </Box>
                                 </FormField>
                             </Box>
