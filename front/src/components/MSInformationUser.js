b015e17c54b514387efc1062ba5fbe5bd5180539
Andres Aleu <andresaleunz@gmail.com>
2022-02-28 11:16:59 +0100

    Add info user

--- front/src/components/MSInformationUser.js
+++ front/src/components/MSInformationUser.js
@@ -1,20 +1,32 @@
 
 import  {Box, Heading, Text} from 'grommet'
+import useUser from '../hooks/useUser'
+import  useLocation  from '../hooks/useLocation'
 
-export default function MSInformationUSer(props) {
+export default function MSInformationUser(props) {
+  const use = useUser()
+ 
+
 
   return(
-<Box
-  direction="column"
-  border={{ color: 'brand', size: 'small' }}
-  pad="large"
->
-<Heading  alignSelf='center' margin={{top:'small'}} level="2">
-  Informacion de usuario
-</Heading>
-<Text margin='none' weight='bold'>Nombre de usuario: {props.name} </Text>
-<Text margin='none' weight='bold'>En {props.location} </Text>
+    <Box
+      direction="column"
+      border={{ color: 'brand', size: 'small' }}
+      pad="large"
+    >
+    <Heading  alignSelf='center' margin={{top:'small'}} level="2">
+      Informacion de usuario
+    </Heading>
+    <Box direction='row' margin={{vertical:'small'}}>
+      <Text margin={{right:'small'}}>Nombre de usuario:</Text>
+      <Text margin='none' weight='bold'> {use.username} </Text>
+    </Box>
+    <Box direction='row'>
+      <Text margin={{right:'small'}}>Email del usuario:</Text>
+      <Text margin='none' weight='bold'> {use.mail} </Text>
+    </Box>
 
-</Box>
+
+    </Box>
   )
 }
